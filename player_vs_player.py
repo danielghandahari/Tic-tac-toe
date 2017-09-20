0c067bef58290ed0d6dcf3b1a47ff3692f4002c0
danielghandahari <dangha94@gmail.com>
2017-09-20 14:25:48 +0200

    Tournament class initialized

--- player_vs_player.py
+++ player_vs_player.py
@@ -5,7 +5,7 @@ board = [" "," "," ",
 		 " "," "," "]
 
 def print_board():
-	"""FUnction to print the board"""
+	"""Function to print the board"""
 	print("======= Your movement =======")
 	print("           " + "-------")
 	print("           " + "|" + board[0] + "|" + board[1] + "|" + board[2] + "|")
@@ -17,7 +17,7 @@ def print_board():
 	print("============================= \n")
 
 def print_board_dummies():
-	"""FUnction to print the board dummies, so the player know the position"""
+	"""Function to print the board dummies, so the player know the position"""
 	print("======= Board Dummies =======")
 	print("           " + "-------")
 	print("           " + "|" + "1" + "|" + "2" + "|" + "3" + "|")
@@ -29,12 +29,12 @@ def print_board_dummies():
 	print("============================= \n")
 
 
-def player_x_move():
+def player_1_move():
 	"""Function to handle player X movement"""
 	print_board()
-	#Capture player X movement
+	#Capture player 1 movement
 	try:
-		move = raw_input("Where do you want to move " + "Player-X" + "? ")
+		move = raw_input("Where do you want to move " + "p1" + "? ")
 		move = int(move) - int(1)
 		#Check if the player inputs valid input i.e 0-8
 		if move > -1 and move < 9:
@@ -43,22 +43,22 @@ def player_x_move():
 				board[move] = "X"
 			else:
 				print("The space is taken!")
-				player_x_move()
+				player_1_move()
 		else:
 			print("Please write valid input i.e 1-9!")
-			player_x_move()
+			player_1_move()
 	#If user enter non-integer input
 	except ValueError:
 		print("Please write valid input i.e 1-9!")
-		player_x_move()
+		player_1_move()
 	
 
-def player_o_move():
+def player_2_move():
 	"""Function to handle player O movement"""
 	print_board()
 	#Capture player O movement
 	try:
-		move = raw_input("Where do you want to move " + "Player-O" + "? ")
+		move = raw_input("Where do you want to move " + "p2" + "? ")
 		move = int(move) - int(1)
 		#Check if the player inputs valid input i.e 0-8
 		if move > -1 and move < 9:
@@ -67,18 +67,18 @@ def player_o_move():
 				board[move] = "O"
 			else:
 				print("The space is taken")
-				player_o_move()
+				player_2_move()
 		else:
 			print("Please write valid input i.e 1-9!")
-			player_x_move()
+			player_2_move()
 	#If user enter non-integer input
 	except ValueError:
 		print("Please write valid input i.e 1-9!")
-		player_x_move()
+		player_2_move()
 
 while True:
 	print_board_dummies()
-	player_x_move()
+	player_1_move()
 
 	#Check if is Tie
 	tie = True
@@ -98,12 +98,12 @@ while True:
 		(board[2] == "X" and board[5] == "X" and board[8] == "X") or \
 		(board[0] == "X" and board[4] == "X" and board[8] == "X") or \
 		(board[2] == "X" and board[4] == "X" and board[6] == "X"):
-		print("Player X win!")
+		print("Congratulation!" + "p1" + "win!")
 		print_board()
 		break
 
 	print_board_dummies()
-	player_o_move()
+	player_2_move()
 
 	#Check if player win
 	if (board[0] == "O" and board[1] == "O" and board[2] == "O") or \
@@ -114,6 +114,6 @@ while True:
 		(board[2] == "O" and board[5] == "O" and board[8] == "O") or \
 		(board[0] == "O" and board[4] == "O" and board[8] == "O") or \
 		(board[2] == "O" and board[4] == "O" and board[6] == "O"):
-		print("Player O win!")
+		print("Congratulation!" + "p2" + "win!")
 		print_board()
 		break
